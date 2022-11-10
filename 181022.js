2c271bb6f8e5814eb8201ea186dd70db0a0e49a8
miftsmkbatikpwr <55234308+miftsmkbatikpwr@users.noreply.github.com>
2022-11-10 13:54:34 +0700

    Upload_10112022
    Ini adalah file yang saya upload pada tanggal  10112022

---
+++ 181022.js
@@ -0,0 +1,11 @@
+let a = 1
+let b = false
+let c = true
+let d = 0
+console.log(a && b)
+console.log(c && b)
+console.log(a && d)
+console.log(a || b)
+console.log(c || b)
+console.log(a || d)
+
---
+++ 191022.js
@@ -0,0 +1,5 @@
+let input = "Teks"
+if (input == "teks") {
+	console.log(input)
+	console.log("SAMA")
+}
